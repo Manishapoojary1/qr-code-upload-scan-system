657b97af05bf22948a0e69e6228bab79f4a8eeb1
Manisha <poojarymanisha134@gmail.com>
2025-12-19 16:45:17 +0530

    Update api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,10 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://qr-backend-nvaj.onrender.com",
+  baseURL: "https://qr-backend-nvaj.onrender.com/api",
+  headers: {
+    "Content-Type": "application/json"
+  }
 });
 
 export default api;
