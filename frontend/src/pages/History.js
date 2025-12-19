b0514134cc27d7f38333edc3818167f36b6b0285
Manisha <poojarymanisha134@gmail.com>
2025-12-19 16:27:11 +0530

    Import api service in History component

--- frontend/src/pages/History.js
+++ frontend/src/pages/History.js
@@ -2,6 +2,7 @@ import axios from "axios";
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import Navbar from "../components/Navbar";
+import api from "../services/api";
 
 function History() {
   const [scans, setScans] = useState([]);
